c27b912b91169506adf0e2c41e249c28b1baab29
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2023-05-30 19:30:37 +0400

    fix validation

--- src/features/personEditForm/model/schema.ts
+++ src/features/personEditForm/model/schema.ts
@@ -1,9 +1,14 @@
 import { FormFieldNames } from '../types/formFieldNames';
 import * as Yup from 'yup';
 
+const INVALID_NUMBER_ERROR = 'Must be a number';
+
 export const schema = Yup.object().shape({
-	[FormFieldNames.Name]: Yup.string().required('Required').typeError('Must be a number'),
-	[FormFieldNames.Height]: Yup.number().min(50).required('Required').typeError('Must be a number'),
-	[FormFieldNames.Mass]: Yup.number().min(1).required('Required').typeError('Must be a number'),
+	[FormFieldNames.Name]: Yup.string().required('Required'),
+	[FormFieldNames.Height]: Yup.number()
+		.min(50)
+		.required('Required')
+		.typeError(INVALID_NUMBER_ERROR),
+	[FormFieldNames.Mass]: Yup.number().min(1).required('Required').typeError(INVALID_NUMBER_ERROR),
 	// ...
 });
